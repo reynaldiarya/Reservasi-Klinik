6a66fa50a034b9676dbe6dc154863aca034739a4
bassp69 <mbintangassp@gmail.com>
2022-08-23 07:37:11 +0700

    Update web.php
    penambahan routes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return "halod";
+});
+
+Route::get('/tes', function () {
+    return "halo";
 });
