e13b59f98ed1d54d5b00c6b29c6dfd1c9d60f9a5
Reynaldi Arya <reynaldiarya.id@gmail.com>
2022-09-02 15:52:30 +0700

    update footer

---
+++ resources/views/login.blade.php
@@ -0,0 +1,14 @@
+@extends('mainTemplate')
+@section('content')
+<div class="container mt-5">
+  <div class="row justify-content-center ">
+    <div class="ms-5 col-sm-4">
+        <div class="d-flex justify-content-center bg-light">
+            ...
+        </div>
+    </div>
+
+   
+  </div>
+</div>
+@endsection
