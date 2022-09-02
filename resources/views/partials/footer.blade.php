f4d03fc93b3aafb9486163f676c8b9a9fc661e08
bassp69 <mbintangassp@gmail.com>
2022-09-02 22:51:17 +0700

    pemecahan navbar dan footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,103 @@
+<!-- Footer -->
+<footer class="text-center text-lg-start bg-dar text-muted mt-5">
+    <!-- Section: Social media -->
+    <section class="container d-flex justify-content-center justify-content-lg-between p-4 border-bottom px-4">
+      <!-- Left -->
+      <div class="me-5 d-none d-lg-block">
+        <span id="foott">Get connected with us on social networks:</span>
+      </div>
+      <!-- Left -->
+  
+      <!-- Right -->
+      <div>
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-whatsapp"></i>
+        </a>
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-facebook-f"></i>
+        </a>
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-twitter"></i>
+        </a>
+        {{-- <a href="" class="me-4 text-reset">
+          <i class="fab fa-google"></i>
+        </a> --}}
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-instagram"></i>
+        </a>
+        {{-- <a href="" class="me-4 text-reset">
+          <i class="fab fa-linkedin"></i>
+        </a> --}}
+        {{-- <a href="" class="me-4 text-reset">
+          <i class="fab fa-github"></i>
+        </a> --}}
+      </div>
+      <!-- Right -->
+    </section>
+    <!-- Section: Social media -->
+  
+    <!-- Section: Links  -->
+    <section class="">
+      <div class="container text-center text-md-start mt-5">
+        <!-- Grid row -->
+        <div class="row mt-3">
+          
+          <div class="col-md-3 col-lg-4 col-xl-1 mx-0 mb-4">
+          <a class="navbar-brand" href="#">
+            <img src="img/logo.png" alt="" width="100">
+          </a>
+          </div>
+  
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+            
+            <!-- Content -->
+            <h6 class="text-uppercase fw-bold mb-4">
+              <i class="fas fa-gem me-3"></i>dr. Reynaldi
+            </h6>
+            <p>
+              Jl Gubeng Kertajaya No 12, Gubeng, Surabaya, Indonesia 12345
+            </p>
+          </div>
+          <!-- Grid column -->
+  
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold mb-4">
+              Links
+            </h6>
+            <p>
+              <a href="/login" class="text-reset">Login</a>
+            </p>
+            <p>
+              <a href="/register" class="text-reset">Register</a>
+            </p>
+          </div>
+          <!-- Grid column -->
+  
+          <!-- Grid column -->
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold mb-4">Contact Us</h6>
+            <p><i class="fas fa-home me-3"></i> Jl Gubeng Kertajaya No 12, Gubeng, Surabaya</p>
+            <p>
+              <i class="fas fa-envelope me-3"></i>
+              info@drrey.com
+            </p>
+            <p><i class="fas fa-phone me-3"></i> + 62 811 222 333</p>
+          </div>
+          <!-- Grid column -->
+        </div>
+        <!-- Grid row -->
+      </div>
+    </section>
+    <!-- Section: Links  -->
+  
+    <!-- Copyright -->
+    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+      © 2022 Copyright:
+      <a class="text-reset" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+    </div>
+    <!-- Copyright -->
+  </footer>
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,25 @@
+<nav class="navbar sticky-top navbar-expand-lg navbar-light bg-transparent ">
+    <div class="container">
+      <a class="navbar-brand" href="#">
+        <img src="img/logo.png" alt="" width="30">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link " aria-current="page" href="/about">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/about#cekJadwal">Reservasi</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link "  href="#foott">Contact Us</a>
+          </li>
+        
+        </ul>
+          <button class="btn btn-outline-info bg-light" type="submit"><a class="nav-link" href="/login">Login</a> </button>
+      </div>
+    </div>
+  </nav>
