9ee2c91510187086a66d130177b2146be1f7a1d8
reynaldiarya <reynaldiarya.id@gmail.com>
2022-09-13 08:13:59 +0700

    front end dashboard

--- routes/web.php
+++ routes/web.php
@@ -31,5 +31,3 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/profile', 'ProfileController@index')->name('profile');
 Route::put('/profile', 'ProfileController@update')->name('profile.update');
-
-
